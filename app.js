25d56c9ec00cb94af60b5ef3b2740416909b81d7
Kartik <kartik.gowda55@gmail.com>
2023-04-10 13:26:10 +1000

    Dynamics posts to post ejs + i -lodash

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
+const express = require('express');
+const _ = require('lodash');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+// //* Load method categories.
+// const array = require('lodash/array');
+// const object = require('lodash/fp/object');
 
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const homeStartingContent =
+  'Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.';
 
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const aboutContent =
+  'Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.';
 
+const contactContent =
+  'Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.';
 
 const app = express();
-const posts = [];
+let posts = [];
 // const posts = [
 //   {
 //     postTitle: "Post 1",
@@ -22,33 +29,54 @@ const posts = [];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get("/", function (req, res) {
-  res.render("home", { homeStartingContent: homeStartingContent });
+app.get('/', function (req, res) {
+  res.render('home', {
+    homeStartingContent: homeStartingContent,
+    posts: posts,
+  });
 });
 
-app.get("/contact", function (req, res) {
-  res.render("contact", { contactContent: contactContent });
+app.get('/contact', function (req, res) {
+  res.render('contact', { contactContent: contactContent });
 });
 
-app.get("/about", function (req, res) {
-  res.render("about", { aboutContent: aboutContent });
+app.get('/about', function (req, res) {
+  res.render('about', { aboutContent: aboutContent });
 });
 
-app.get("/compose", function (req, res) {
-  res.render("compose");
+app.get('/compose', function (req, res) {
+  res.render('compose');
 });
 
-app.post("/compose", function (req, res) {
+app.get('/posts/:postName', function (req, res) {
+  const requestedTitle = _.lowerCase(req.params.postName);
+  posts.forEach(function (post) {
+    const storedTitle = _.lowerCase(post.postTitle);
+    if (storedTitle === requestedTitle) {
+      console.log('Match found!');
+      res.render('post', { posts: posts });
+    }
+    else {
+      console.log('No match found!');
+    }
+  }
+  );
+});
+
+app.post('/compose', function (req, res) {
   const post = {
     postTitle: req.body.postTitle,
     postBody: req.body.postBody,
-  }
+  };
   posts.push(post);
-  res.render("home", { homeStartingContent: homeStartingContent, posts: posts });
+  res.render('home', {
+    homeStartingContent: homeStartingContent,
+    posts: posts,
+  });
 });
 
 app.listen(3000, function () {
-  console.log("Server started on port 3000");
+  console.log('Server started on port 3000');
 });
